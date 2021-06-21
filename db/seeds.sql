059b5d0bc4432a0a5e5a5c6ef56604c9f3946de7
truethatplaya <alina.hernandez.ca@gmail.com>
2021-06-21 00:15:25 -0700

    added roles to seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,35 @@ VALUES ("Joint Rules");
 
 INSERT INTO department (name)
 VALUES ("IT");
+
+//roles
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Chief", 125000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Assemblymember", 125500, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Senator", 135000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Leg Aid", 60400, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Consultant", 100800, 5);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("IT Spec 1", 90600, 6);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Council", 110300, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Leg Director", 80500, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("HR Rep", 85000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Floor", 56700, 5);
