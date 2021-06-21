cc7027e149659e1eb294c832c623fa79805b2bbb
truethatplaya <alina.hernandez.ca@gmail.com>
2021-06-21 00:06:46 -0700

    added departments to seed

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES ("Assembly");
+
+INSERT INTO department (name)
+VALUES ("Senate");
+
+INSERT INTO department (name)
+VALUES ("HR");
+
+INSERT INTO department (name)
+VALUES ("Legal");
+
+INSERT INTO department (name)
+VALUES ("Joint Rules");
+
+INSERT INTO department (name)
+VALUES ("IT");
