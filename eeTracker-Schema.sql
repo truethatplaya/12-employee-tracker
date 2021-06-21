55240706f8634cbdd302a9594c7f293fcf2ba206
truethatplaya <alina.hernandez.ca@gmail.com>
2021-06-20 22:28:42 -0700

    database is connects to workbench

---
+++ eeTracker-Schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS ee_trackerDB;
+CREATE database ee_trackerDB;
+
+USE ee_trackerDB;
+
+-- CREATE TABLE ee (
+--   position INT NOT NULL,
+--   artist VARCHAR(100) NULL,
+--   song VARCHAR(100) NULL,
+--   year INT NULL,
+--   raw_total DECIMAL(10,4) NULL,
+--   raw_usa DECIMAL(10,4) NULL,
+--   raw_uk DECIMAL(10,4) NULL,
+--   raw_eur DECIMAL(10,4) NULL,
+--   raw_row DECIMAL(10,4) NULL,
+--   PRIMARY KEY (position)
+-- );
