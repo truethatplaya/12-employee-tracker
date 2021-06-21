4db8d728b03c7403439e855679817f6a55d7ad3a
truethatplaya <alina.hernandez.ca@gmail.com>
2021-06-20 23:06:47 -0700

    created employee table and role table.

--- eeTracker-Schema.sql
+++ eeTracker-Schema.sql
@@ -5,6 +5,22 @@ USE ee_trackerDB;
 
 CREATE TABLE department(
     id INT AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT NULL,
+  PRIMARY KEY (id),
+);
+
+CREATE TABLE role (
+  id INT
+  title VARCHAR(30),
+  salary DECIMAL (6, 2),
+  department_id INT,
 );
