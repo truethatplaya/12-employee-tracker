76fe130a04a6e807ba5380a80939ded9b47ee791
truethatplaya <alina.hernandez.ca@gmail.com>
2021-06-20 23:57:30 -0700

    added foreign keys to tables

--- eeTracker-Schema.sql
+++ eeTracker-Schema.sql
@@ -10,17 +10,21 @@ CREATE TABLE department(
 );
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
   PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES rode(id),
+  FOREIGN KEY (manager_id) REFERENCES rode(id),
 );
 
 CREATE TABLE role (
-  id INT
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL (6, 2),
   department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department (id),
+  PRIMARY KEY (id)
 );
